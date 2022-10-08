843d6638a110edb21808f3f61d65819a5c7115b3
DESKTOP-8H5ME3U\남상우 <deedee0905@naver.com>
2022-10-08 12:44:11 +0900

    [남상아]  - 창작 컨텐츠 화면구성 단일 페이지(select) / 진행중
    deedee0905/CreateType#14

--- src/main/java/com/amita/createType/ex/post/PostController.java
+++ src/main/java/com/amita/createType/ex/post/PostController.java
@@ -8,23 +8,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/post")
 public class PostController {
 	
+	// 크리에이트 타입 메인 view
 	@GetMapping("/main/view")
 	public String createTypeMainView() {
 		
 		return "post/mainView";
 	}
 	
+	// 창작 컨텐츠 전체 view
 	@GetMapping("/createList/view")
 	public String createListView() {
 		
 		return "post/createContents";
 	}
 	
+	// 포스트 발행 view
 	@GetMapping("/create/newPost/view")
 	public String newPostView() {
 		return "post/newPost";
 	}
 	
+	// 포스트 단일 view
 	@GetMapping("/create/postObject/view")
 	public String postObject() {
 		return "post/postView";
---
+++ src/main/java/com/amita/createType/ex/post/model/Post.java
@@ -0,0 +1,80 @@
+package com.amita.createType.ex.post.model;
+
+import java.util.Date;
+
+public class Post {
+	
+	private int id;
+	private int userId;
+	private int channelId;
+	private String title;
+	private String subtitle;
+	private String content;
+	private int category;
+	private int price;
+	private Date createdAt;
+	private Date updatedAt;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getChannelId() {
+		return channelId;
+	}
+	public void setChannelId(int channelId) {
+		this.channelId = channelId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSubtitle() {
+		return subtitle;
+	}
+	public void setSubtitle(String subtitle) {
+		this.subtitle = subtitle;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getCategory() {
+		return category;
+	}
+	public void setCategory(int category) {
+		this.category = category;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+}
